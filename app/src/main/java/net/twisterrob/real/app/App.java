42e3f4c8b318073efa485e8ba054e9247ac32e68
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2019-02-03 23:04:10 +0000

    butterknife-reflect

--- app/src/main/java/net/twisterrob/real/app/App.java
+++ app/src/main/java/net/twisterrob/real/app/App.java
@@ -1,5 +1,7 @@
 package net.twisterrob.real.app;
 
+import java.lang.reflect.*;
+
 import android.app.Application;
 import android.content.Context;
 import android.support.multidex.MultiDex;
@@ -9,11 +11,53 @@ public class App extends Application {
 	@Override protected void attachBaseContext(Context base) {
 		super.attachBaseContext(base);
 		MultiDex.install(this); // [multidex]
+
+		System.out.println("ButterKnife: " + ButterKnifeDetector.butterknifeBuild());
 	}
 
 	@Override public void onCreate() {
 		super.onCreate();
 
 		Injectors.init(this);
+	}
+}
+
+class ButterKnifeDetector {
+
+	@SuppressWarnings("StaticMethodOnlyUsedInOneClass")
+	static String butterknifeBuild() {
+		if (isButterKnifeReal() && isButterKnifeReflect()) {
+			return "inconclusive";
+		} else if (isButterKnifeReal()) {
+			return "compiled";
+		} else if (isButterKnifeReflect()) {
+			return "reflect";
+		} else {
+			return "none";
+		}
+	}
+
+	private static boolean isButterKnifeReal() {
+		try {
+			Class<?> ButterKnife = Class.forName("butterknife.ButterKnife");
+			@SuppressWarnings("unused")
+			Field BINDINGS = ButterKnife.getDeclaredField("BINDINGS");
+			return true;
+		} catch (ClassNotFoundException ignored) {
+		} catch (NoSuchFieldException ignored) {
+		}
+		return false;
+	}
+
+	private static boolean isButterKnifeReflect() {
+		try {
+			Class<?> ButterKnife = Class.forName("butterknife.ButterKnife");
+			@SuppressWarnings({"unused", "JavaReflectionMemberAccess"})
+			Method trySet = ButterKnife.getDeclaredMethod("trySet", Field.class, Object.class, Object.class);
+			return true;
+		} catch (ClassNotFoundException ignored) {
+		} catch (NoSuchMethodException ignored) {
+		}
+		return false;
 	}
 }
